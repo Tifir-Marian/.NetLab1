c95c460dd8b675522f1f9cc176256e911b8556d5
Tifir <maria_vladut52@yahoo.com>
2020-04-01 14:24:14 +0300

    laborator

---
+++ Counter.cs
@@ -0,0 +1,36 @@
+﻿using System;
+
+namespace dotNet
+{
+    class Counter
+    {
+
+        private int threshold;
+        private int total;
+
+        public Counter(int passedThreshold)
+        {
+            threshold = passedThreshold;
+        }
+
+        public void Add(int x)
+        {
+            total += x;
+            if (total >= threshold)
+            {
+                OnThresholdReached(EventArgs.Empty);
+            }
+        }
+
+        protected virtual void OnThresholdReached(EventArgs e)
+        {
+            EventHandler handler = ThresholdReached;
+            if (handler != null)
+            {
+                handler(this, e);
+            }
+        }
+
+        public event EventHandler ThresholdReached;
+    }
+}
